c1b9132d05fc40fd61cdbd47a1906dfc1a26881d
rk463345 <ryan.karpinski2@gmail.com>
2018-09-17 17:15:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from block import Block
+import os
 #from chain import Chain
 
-x = Block("this is some random text")
+block1 = Block()
+while True:
+  data_directory = input("enter the directory of the file to be parsed and added to the blockchain:\n")
+  if os.path.isfile(data_directory):
+    print("valid path")
+    break
+data  = open(data_directory)
+while data.hasNext()
+  block1.add_transaction(data.readline())
+data.close()
 x.print_block()
 
