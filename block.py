75d013a51c3a42b8a44d6d6c3dbf2a082f12e4f1
rk463345 <ryan.karpinski2@gmail.com>
2018-09-17 16:58:08 -0500

    Update block.py

--- block.py
+++ block.py
@@ -8,39 +8,44 @@ import json #used for strings in order for them to be cross programming language
 
 class Block:
 
-    def __init__(self, parsed_data, p_transaction = ""):
+    def __init__(self,  p_transaction = ""):
         """Initializes the Block data structure"""
         self.pTransaction = p_transaction  # contains the previous transaction hash
-        self.parsedData = parsed_data    # holds the data parsed for it to be hashed
-        self.jsonData = ""  # put here for testing, most likely will be removed
+        self.parsedData = []   # holds the data parsed for it to be hashed
         self.seed = urandom(19) # random pulled from system call
         self.vHash = "" # holds the verification hash
-        self.cTransaction = "" # contains the current transaction hash  MAY REMOVE AND USE vHash
-        self.final_hash = self.create_hash() # final hash to be put into the chain
+        self.bloc = []
+        self.final_hash = "" # final hash to be put into the chain
 
-    def create_hash(self):
+    def package_block(self):
         """checks to make sure data is in type str and then hashes with SHA-256 and returns hash"""
         if len(self.pTransaction) == "":
             # check to see if there is a previous transaction, if not generates a random string to be hashed
             self.pTransaction = getrandbits(256)
             # gets random bits to be hashed in case of being first block
         if type(self.parsedData) != str:    # make sure parsed data is in string form
-            self.jsonData = json.dumps(self.parsedData, sort_keys=True)
+            self.parsedData = json.dumps(self.parsedData, sort_keys=True)
             # makes all pieces of data parsed form file to type str
-        to_hash = str(self.seed) + str(self.pTransaction) + str(self.parsedData) + str(self.parsedData)
-        # creates a string for final hash
-        self.final_hash = hashlib.sha256(str(to_hash).encode('utf-8')).hexdigest()
-        # creates final hash
+        #to_hash = str(self.seed) + str(self.pTransaction) + str(self.parsedData) + str(self.parsedData)
         self.vHash = hashlib.sha256(str(self.parsedData).encode('utf-8')).hexdigest()
         # hashes the transaction data with sha-256 for a string encoded with utf-8
-        return hashlib.sha256(str(to_hash).encode('utf-8')).hexdigest()
-        # creates and returns final hash
-
+        self.bloc.append(self.pTransaction)
+        self.bloc.append(self.parsedData)
+        self.bloc.append(self.seed)
+        self.block.append(self.vHash)
+        # creates a string for final hash
+        self.final_hash = hashlib.sha256(str(self.bloc).encode('utf-8')).hexdigest()
+        # creates final hash
+        print("success, block made with {} transaction(s)").format(len(self.bloc))
+       
+    def add_transaction(self, transaction):
+        self.parsedData.append(transaction)
+    
     def get_ptransaction(self):
         """returns the hash of the previous transaction"""
         return json.dumps(self.pTransaction, sort_keys=True)
     
-    def get_ctransaction(self):
+    def get_verification_hash(self):
         """returns the has of the current transaction"""
         return json.dumps(self.cTransaction, sort_keys=True)
 
@@ -54,7 +59,7 @@ class Block:
 
     def print_block(self):
         """prints the data of the block, most likely will be removed"""
-        print(self.cTransaction)
+        print(self.vHash)
         print(self.pTransaction)
         print(self.parsedData)
         print(self.seed)
