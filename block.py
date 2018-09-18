472576d2b8daf94b97b51f4b820480519d3dd453
rk463345 <ryan.karpinski2@gmail.com>
2018-09-18 11:50:59 -0500

    made more theatrical for demo

--- block.py
+++ block.py
@@ -1,6 +1,8 @@
 # A simple block for concept demo
 
 from random import getrandbits
+import uuid
+import time
 import hashlib  #needed to be able to hash the data with SHA-256
 from os import urandom   #needed for the random seed
 import json #used for strings in order for them to be cross programming language compatible
@@ -19,7 +21,7 @@ class Block:
 
     def package_block(self):
         """checks to make sure data is in type str and then hashes with SHA-256 and returns hash"""
-        if len(self.pTransaction) == "":
+        if len(self.pTransaction) < 2:
             # check to see if there is a previous transaction, if not generates a random string to be hashed
             self.pTransaction = getrandbits(256)
             # gets random bits to be hashed in case of being first block
@@ -32,11 +34,11 @@ class Block:
         self.bloc.append(self.pTransaction)
         self.bloc.append(self.parsedData)
         self.bloc.append(self.seed)
-        self.block.append(self.vHash)
+        self.bloc.append(self.vHash)
         # creates a string for final hash
         self.final_hash = hashlib.sha256(str(self.bloc).encode('utf-8')).hexdigest()
         # creates final hash
-        print("success, block made with {} transaction(s)").format(len(self.bloc))
+        #print("success, block made with 1 transaction(s)")
        
     def add_transaction(self, transaction):
         self.parsedData.append(transaction)
@@ -47,26 +49,52 @@ class Block:
     
     def get_verification_hash(self):
         """returns the has of the current transaction"""
-        return json.dumps(self.cTransaction, sort_keys=True)
+        return json.dumps(self.vHash, sort_keys=True)
+
+    def get_final_hash(self):
+        return self.final_hash
 
     def verify_block(self):
         """verifies that the block contents were not manipulated by comparing to hash"""
-        if hashlib.sha256(str(self.parsedData).encode('utf-8')).hexdigest() == self.cTransaction:
-            print('success')
+        if hashlib.sha256(str(self.parsedData).encode('utf-8')).hexdigest() == self.vHash:
+            print('success, correct data found')
         else:
             print('contents are not identical')
         #   TODO WRITE THIS FUNCTION
 
     def print_block(self):
         """prints the data of the block, most likely will be removed"""
+        print("Starting Work on block")
+        for num in range(0, 13):
+            print(hashlib.sha256(str(num).encode('utf-8')).hexdigest())
+            time.sleep(1)
+        #print(hashlib.sha256("1".encode('utf-8')).hexdigest())
+        #time.sleep(1)
+        #print(hashlib.sha256("2".encode('utf-8')).hexdigest())
+        #time.sleep(1)
+        #print(hashlib.sha256("3".encode('utf-8')).hexdigest())
+        #time.sleep(1)
+        #print(hashlib.sha256("4".encode('utf-8')).hexdigest())
+        #time.sleep(1)
         print(self.vHash)
-        print(self.pTransaction)
-        print(self.parsedData)
-        print(self.seed)
-        print(self.final_hash)
+        time.sleep(1)
         self.verify_block()
+        time.sleep(2)
+        print("\n\nCONTENTS OF BLOCK")
+        print("\nBlock ID")
+        print(uuid.uuid4())
+        print("\nVerification Hash:")
+        print(self.vHash)
+        print("\nPrevious transaction Hash:")
+        print(self.pTransaction)
+        print("\nCurrent transactions")
+        print(self.parsedData)
+        print("\nThe random Seed in the block:")
+        print(self.seed)
+        print("\nThe final has of the entire block:")
+        print(self.final_hash)
 
     def return_un_hashed_set(self):
         """returns the list of data that will be hashed together for the final hash before being put onto the chain"""
-        full_set = [self.pTransaction, self.parsedData, self.seed, self.cTransaction]
+        full_set = [self.pTransaction, self.parsedData, self.seed, self.vHash]
         return full_set
--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 from block import Block
+import time
 import os
 #from chain import Chain
 
+print("searching for peers")
+time.sleep(3)
+print("connecting to peers")
+time.sleep(5)
+print("connected, receiving work")
+time.sleep(2)
+print("Ledger and work received")
 block1 = Block()
-while True:
+block1.add_transaction("my medical records")
+block1.package_block()
+"""while True:
   data_directory = input("enter the directory of the file to be parsed and added to the blockchain:\n")
   if os.path.isfile(data_directory):
     print("valid path")
     break
-data  = open(data_directory)
-while data.hasNext()
-  block1.add_transaction(data.readline())
-data.close()
-x.print_block()
+with  open(data_directory) as data:
+  transactions = data.readlines()
+  for line in range(0, len(transactions)):
+    block1.add_transaction(line)
+data.close()"""
+block1.print_block()
 
