a48dc2485ef1dbde7a2db00fd489e84601107ff5
rk463345 <ryan.karpinski2@gmail.com>
2018-10-20 00:05:03 -0500

    created several classes and python files

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 # driver for Connect4Block
 import sys
 import connect4
-import crackhash
 import ledger
-from chain import Chain
-
+import os
+# todo make ledger actually use previous hash function for ledger with genesis blocks
+# todo fix search function
+# todo modify and implement p2p
+# todo research HIPA compliance possibly add in encryption for ledger
 
 def find_patient():
     connect4.clear()
-    input('fEnter SSN to get patient information:\n')
+    ssn = input('fEnter SSN to get patient information:\n')
+    data = ledger.search_ledger(ssn)
+    for item in data:
+        print(item)
 
 def add_transaction():
     data = input("Enter the directory for file to be added to the block chain:\n")
-
-    connect4.create_block(data)
-
-def mine():
-    crackhash.mine_hash()
+    if os.path.exists(data):
+        connect4.create_block(data)
+    else:
+        print('Invalid directory')
 
 def main():
     while True:
@@ -24,7 +28,7 @@ def main():
         print("What would you like to do?\n"
              "Add transaction = 1\n"
             "Mine blocks = 2\n"
-            "Find Data = 3\n"
+            "Find Patient (currently not working)= 3\n"
             "exit = q")
         print("*" * 18)
 
@@ -32,11 +36,12 @@ def main():
         if choice == "1":
             add_transaction()
         elif choice == "2":
-            mine()
-        elif choice =="2":
+            connect4.cracking()
+        elif choice =="3":
             find_patient()
         elif choice == "q":
             print("EXITING")
+            connect4.writechain()
             sys.exit()
         else:
             connect4.clear()
